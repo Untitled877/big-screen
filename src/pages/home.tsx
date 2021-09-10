6de0650edf1abefe1efb65079fce8d0c73b24554
Untitled877 <1113425327@qq.com>
2021-09-11 00:30:28 +0800

    finish basic layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,10 +4,15 @@ import headerBg from '../images/header.png';
 
 export const Home = () => {
   return (
-    <div>
-      <div className="home">
-        <header style={{backgroundImage: `url(${headerBg})`}} />
-      </div>
+    <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}/>
+      <main>
+        <section className="section1"/>
+        <section className="section2"/>
+        <section className="section3"/>
+        <section className="section4"/>
+        <section className="section5"/>
+      </main>
     </div>
   );
 };
