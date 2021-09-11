09607e7be9513cf4bce18f4db8b5fb49f60b57ee
Untitled877 <1113425327@qq.com>
2021-09-11 17:21:58 +0800

    重构 chart-1 封装函数

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
-import { px } from '../shared/px';
-import {createEchartsOptions} from '../shared/create-echarts-options';
 
-export const Chart1 = () => {
+const px = (n) => n / 2420 * (window as any).pageWidth;
+export const Chart2 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     let myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
+    myChart.setOption({
+      textStyle: {
+        fontSize: px(12),
+        color: '#79839E'
+      },
+      title: {show: false},
+      legend: {show: false},
       xAxis: {
         data: ['城关区', '七里河区', '西固区', '安宁区', '红谷区', '永登区', '皋兰区', '榆中区', '兰州新区'],
         axisTick: {show: false},
@@ -15,6 +20,7 @@ export const Chart1 = () => {
           lineStyle: {color: '#083B70'}
         },
         axisLabel: {
+          fontSize: px(12),
           margin: px(8),
           formatter(val) {
             if (val.length > 2) {
@@ -27,6 +33,12 @@ export const Chart1 = () => {
           }
         },
       },
+      grid: {
+        x: px(40),
+        y: px(40),
+        x2: px(40),
+        y2: px(40),
+      },
       yAxis: {
         splitLine: {show: false},
         axisLine: {
@@ -34,6 +46,7 @@ export const Chart1 = () => {
           lineStyle: {color: '#083B70'}
         },
         axisLabel: {
+          fontSize: px(12),
           margin: px(8)
         }
       },
@@ -41,7 +54,7 @@ export const Chart1 = () => {
         type: 'bar',
         data: [40, 30, 25, 16, 23, 13, 18, 27, 32]
       }]
-    }));
+    });
   }, []);
 
 
