ee2dbfe3a997b861aa885c8cf64cd1f65fea2e36
Untitled877 <1113425327@qq.com>
2021-09-12 21:58:40 +0800

    chart9 finished

--- src/components/chart-9.tsx
+++ src/components/chart-9.tsx
@@ -8,50 +8,74 @@ export const Chart9 = () => {
   useEffect(() => {
     let myChart = echarts.init(divRef.current);
     myChart.setOption(createEchartsOptions({
-      xAxis: {show: false},
-      yAxis: {show: false},
-      legend: {show: false},
+      color: '#f39f14',
+      legend: {
+        bottom: px(-10),
+        textStyle: {color: '#7fa5c6'},
+        itemWidth: px(30),
+        itemHeight: px(16),
+        itemGap:px(5)
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['', '28', '38', '48', '58', '68', '78'],
+        axisTick: {show: false},
+        axisLine: {show: false},
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#083970',
+          }
+        },
+        axisLabel: {
+          margin: px(8),
+        }
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#083970'
+          }
+        },
+        axisLabel: {
+          margin: px(4),
+          formatter(val) {
+            return val * 100 + '%';
+          }
+        }
+      },
       series: [
         {
-          type: 'pie',
-          radius: ['65%', '80%'],
-          avoidLabelOverlap: false,
-          label: {
-            show: true,
-            position: 'inside',
-            textStyle: {
-              color:'white',
-              fontSize: px(20)
-            },
-            formatter(options) {
-              return options.value * 100 + '%';
-            }
-          },
-          labelLine: {show:false},
-          itemStyle: {
-            borderColor: '#0F113A',
-            borderWidth: px(4)
-          },
-          data: [
-            {value: 0.20, name: '女'},
-            {value: 0.80, name: '男'}
-          ]
-        }
-      ]
+          name: '盗窃',
+          type: 'line',
+          data: [0.2, 0.22, 0.28, 0.36, 0.26, 0.19, 0.13, 0.11],
+          symbol: 'circle',
+          symbolSize: px(10),
+          lineStyle: {width: px(2)},
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              offset: 0,
+              color: '#f39f14'
+            }, {
+              offset: 1,
+              color: '#1b1d52'
+            }]),
+          }
+        },
+      ],
+
     }));
   }, []);
 
-
   return (
-    <div className="年龄段分布-图一">
-      <div className="chart">
-        <div className="main" ref={divRef}/>
-        <div className="text">性别</div>
-      </div>
-      <div className="legend">
-        <span className="male"/>男
-        <span className="female"/>女
-      </div>
+  <div className="年龄段分布-图三">
+    <h3>犯罪年龄趋势图</h3>
+    <div ref={divRef} className="chart">
+
     </div>
+  </div>
   );
 };
